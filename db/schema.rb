83d19986242296a1976bba25c02740754a3a966d
Kazuki10peace <kazuki10asobo@ezweb.ne.jp>
2022-01-28 12:51:47 +0900

    テーブルカラムの修正

--- db/schema.rb
+++ db/schema.rb
@@ -18,9 +18,9 @@ ActiveRecord::Schema.define(version: 2022_01_24_092749) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "nickname", null: false
     t.string "last_name", null: false
     t.string "first_name", null: false
     t.string "last_name_kana", null: false
