d60a4e1abca854afa9692a65e5ffbb313d338650
Kazuki10peace <kazuki10asobo@ezweb.ne.jp>
2022-01-28 17:55:37 +0900

    パスワード英数字混合必須設定に記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
 
 
   validates :nickname, presence: true
+  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  validates :encrypted_password, format: { with: VALID_PASSWORD_REGEX }
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :last_name_kana, presence: true
