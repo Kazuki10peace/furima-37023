c975b17e95781571bab07fd2f8def9e571b2f2f1
Kazuki10peace <kazuki10asobo@ezweb.ne.jp>
2022-01-28 20:35:18 +0900

    FactoryBot修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,10 +2,10 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Lorem.characters(min_alpha: 1, min_numric: 1, min_length: 6)}
+    password              {Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1)}
     password_confirmation {password}
-    last_name             {Gimei.name.last.kanji.hiragana.katakana}
-    first_name            {Gimei.name.first.kanji.hiragana.katakana}
+    last_name             {Gimei.last.kanji}
+    first_name            {Gimei.first.kanji}
     last_name_kana        {Gimei.last.katakana}
     first_name_kana       {Gimei.first.katakana}
     birth_date            {Faker::Date.birthday(min_age: 18, max_age: 65)}
