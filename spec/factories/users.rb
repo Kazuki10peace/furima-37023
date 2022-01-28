2b034e56bcb16023c3b104bbe2d6ef4ae75bae08
Kazuki10peace <kazuki10asobo@ezweb.ne.jp>
2022-01-28 19:59:42 +0900

    FactoryBot追記

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,10 @@ FactoryBot.define do
     email                 {Faker::Internet.free_email}
     password              {Faker::Lorem.characters(min_alpha: 1, min_numric: 1, min_length: 6)}
     password_confirmation {password}
+    last_name             {}
+    first_name            {}
+    last_name_kana        {}
+    first_name_kana       {}
+    birth_date            {}
   end
 end
