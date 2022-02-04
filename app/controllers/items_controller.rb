b98b9e7c3cf8a5228249a88886e47f1ec711860f
Kazuki10peace <kazuki10asobo@ezweb.ne.jp>
2022-02-04 19:57:05 +0900

    ビューファイル編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,17 @@
 class ItemsController < ApplicationController
 
   def index
+    @item = Item.all
   end
 
   def new
+    @item = Item.new
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :info, :category_id, :sales_status_id, :shipping_fee_id, :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
   end
 
 end
