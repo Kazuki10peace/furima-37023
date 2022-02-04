24d8e5435229c628f8586b63258389ed0bab5889
Kazuki10peace <kazuki10asobo@ezweb.ne.jp>
2022-02-04 14:21:10 +0900

    バリデーション記述（一部）

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+
+  validates :name, precence: true
+  validates :info, precence: true
+  validates :category, precence: true
+  validates :sales_status, precence: true
+  validates :shipping_fee, precence: true
+  validates :prefecture, precence: true
+  validates :scheduled_delivery, precence: true
+  PRICE_REGEX = /^[0-9]+$/.freeze
+  validates_format_of :price, precence: true, numericality: { in: 300..9999999 }, with: PRICE = REGEX
 end
