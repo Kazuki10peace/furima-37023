3b9f8b77c5873c91e464c4822925d05785fe69b7
Kazuki10peace <kazuki10asobo@ezweb.ne.jp>
2022-02-05 19:49:17 +0900

    レビュー後修正

--- app/javascript/price_calculation.js
+++ app/javascript/price_calculation.js
@@ -1,7 +1,6 @@
 window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
-    const inputValue = priceInput.value;
     const addTaxDom = document.getElementById("add-tax-price");
     addTaxDom.innerHTML = Math.round(priceInput.value * 0.1 );
     const addPofitDom = document.getElementById("profit");
