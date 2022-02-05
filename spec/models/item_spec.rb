ed7bb559204ccfe923a663b6a2d0d9b241198fda
Kazuki10peace <kazuki10asobo@ezweb.ne.jp>
2022-02-05 12:26:18 +0900

    微修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,14 +2,19 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   before do
-    @item = FactoryBot.build(:item)
+    @item = FactoryBot.create(:item)
   end
 
   describe '商品出品機能' do
     context '出品登録がうまくいくとき' do
       it 'image,name,info,category_id,sales_status_id,shipping_fee_id,prefecture_id,scheduled_delivery_id,priceが存在すれば登録できる' do
-        expect(@item).to be_valid
       end
+      it 'priceが300~9,999,999の場合のみ保存できること'
+      end
+    end
+
+    context '出品機能がうまくいかないとき' do
+      it 'imageが空だと登録できない'
     end
   end
 end
