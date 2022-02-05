48e952e46c35bea649ac98be4926d7abe4c50d50
Kazuki10peace <kazuki10asobo@ezweb.ne.jp>
2022-02-05 12:03:09 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,7 @@ class Item < ApplicationRecord
   validates :prefecture_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :scheduled_delivery_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   PRICE_REGEX = /^[0-9]+$/.freeze
-  validates_format_of :price, presence: true, numericality: { in: 300..9999999 }, with: PRICE = REGEX
+  validates_format_of :price, with: PRICE_REGEX, presence: true, numericality: { in: 300..9999999 },
 
   #まだ先の実装の分はコメントアウト
   belings_to :user
