76a5d48327e40b33e3df316cbabe3fa5e8a83550
Kazuki10peace <kazuki10asobo@ezweb.ne.jp>
2022-02-06 16:31:52 +0900

    コントローラー編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
 
   def index
-    # @item = Item.all 一覧表示実装時外す
+    @items = Item.all
   end
 
   def new
