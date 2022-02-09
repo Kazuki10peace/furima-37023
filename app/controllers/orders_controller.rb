652816e40d4406dd2520c4329ddf68fe99749446
Kazuki10peace <kazuki10asobo@ezweb.ne.jp>
2022-02-09 19:38:08 +0900

    ordersコントローラー記述

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,40 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
+
+  def index
+    @orders = Order.new
+    @item = Item.find(params[:item_id])
+    @address = Address.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:price).merge(token: params[:token])
+  end
+
+  def address_params
+    params.permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(order_id: @order.id)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: order_params[:price],
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
+
+end
