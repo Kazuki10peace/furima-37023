300d02b55e37ab849100e00c21c36397f59468e8
Kazuki10peace <kazuki10asobo@ezweb.ne.jp>
2022-02-18 16:29:30 +0900

    コントローラー編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,41 +1,41 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index]
+  before_action :set_item, only: [:index,:create]
 
   def index
-    @order_history = OrderHistory.new
-    @item = Item.find(params[:item_id])
-    @address = Address.new
-    redirect_to root_path if current_user.id == @item.user.id
-  end
-
-  def new
     @order_history_address = OrderHistoryAddress.new
   end
 
   def create
-    @order_history_address = OrderHistoryAddress.new(order_history_params)
+    @order_history_address = OrderHistoryAddress.new(address_params)
     if @order_history_address.valid?
       pay_item
       @order_history_address.save
       redirect_to root_path
     else
-      render 'index'
+      render :index
     end
   end
 
   private
 
-  def order_history_params
-    params.require(:order_history_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(
-      user_id: current_user.id, token: params[:token]
+  def address_params
+    params.require(:order_history_address).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :order_history_id).merge(
+      item_id: params[:item_id],user_id: current_user.id,token: params[:token])
+  end
+
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price, 
+      card: address_params[:token],
+      currency: 'jpy' 
     )
   end
 
-  def pay_item
-    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
-    Payjp::Charge.create(
-      card: order_history_params[:token],
-      currency: 'jpy'
-    )
+  def set_item
+    @item = Item.find(params[:item_id])
   end
+
 end
