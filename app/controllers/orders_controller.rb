a3d64a4c4fedfc2ad2eea95f9aa0cab1ec220b4a
Kazuki10peace <kazuki10asobo@ezweb.ne.jp>
2022-02-18 18:16:01 +0900

    購入済商品ページへ立入禁止

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,6 +3,9 @@ class OrdersController < ApplicationController
   before_action :set_item, only: [:index, :create]
 
   def index
+    if @item.user_id == current_user.id || @item.order_history != nil
+      redirect_to root_path
+    end
     @order_history_address = OrderHistoryAddress.new
   end
 
