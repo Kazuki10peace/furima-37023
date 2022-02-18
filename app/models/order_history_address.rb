b5afc09f76f7b4a2c1bdf41b46f83a9f1306c2f3
Kazuki10peace <kazuki10asobo@ezweb.ne.jp>
2022-02-18 17:10:37 +0900

    バリデーション記述

--- app/models/order_history_address.rb
+++ app/models/order_history_address.rb
@@ -3,12 +3,11 @@ class OrderHistoryAddress
   attr_accessor :postal_code, :token, :prefecture_id, :city, :addresses, :building, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
-    validates :token
-    validates :postal_code
-    validates :prefecture_id
-    validates :city
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
     validates :addresses
-    validates :phone_number
+    validates :phone_number, format: { with: /\A\d{11}\z/}
   end
 
   def save
