4d34f4e23a13ba9a006b9e620e606187586377c5
Kazuki10peace <kazuki10asobo@ezweb.ne.jp>
2022-02-18 16:28:47 +0900

    ordersモデル削除

--- db/schema.rb
+++ db/schema.rb
@@ -70,12 +70,6 @@ ActiveRecord::Schema.define(version: 2022_02_09_041326) do
     t.index ["user_id"], name: "index_order_histories_on_user_id"
   end
 
-  create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "price", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
