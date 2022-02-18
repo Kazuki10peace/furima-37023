a9bbb444f52092e41f796ff10fa0a736761656f4
Kazuki10peace <kazuki10asobo@ezweb.ne.jp>
2022-02-18 18:18:44 +0900

    クレジット入力欄のバリデーション

--- app/models/order_history_address.rb
+++ app/models/order_history_address.rb
@@ -3,6 +3,7 @@ class OrderHistoryAddress
   attr_accessor :postal_code, :token, :prefecture_id, :city, :addresses, :building, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
+    validates :token
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
