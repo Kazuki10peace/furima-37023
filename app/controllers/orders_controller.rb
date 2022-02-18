975d4637dbbca72791ca2cf7589dedf9fe2e78fd
Kazuki10peace <kazuki10asobo@ezweb.ne.jp>
2022-02-18 18:01:30 +0900

    エラーメッセージ追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index]
-  before_action :set_item, only: [:index,:create]
+  before_action :set_item, only: [:index, :create]
 
   def index
     @order_history_address = OrderHistoryAddress.new
@@ -8,7 +8,6 @@ class OrdersController < ApplicationController
 
   def create
     @order_history_address = OrderHistoryAddress.new(address_params)
-    binding.pry
     if @order_history_address.valid?
       pay_item
       @order_history_address.save
@@ -22,13 +21,13 @@ class OrdersController < ApplicationController
 
   def address_params
     params.require(:order_history_address).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :order_history_id).merge(
-      item_id: params[:item_id],user_id: current_user.id,token: params[:token])
+      item_id: params[:item_id], user_id: current_user.id, token: params[:token])
   end
 
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
-      amount: @item.price, 
+      amount: Item.find(params[:item_id]).price,
       card: address_params[:token],
       currency: 'jpy' 
     )
