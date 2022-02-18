b98bbb736cecc80774aef84aa3a1324b40f66e34
Kazuki10peace <kazuki10asobo@ezweb.ne.jp>
2022-02-18 16:28:32 +0900

    jabascript編集

--- app/javascript/price_calculation.js
+++ app/javascript/price_calculation.js
@@ -1,9 +1,10 @@
 window.addEventListener('load', () => {
-  const priceInput = document.getElementById("item-price");
+  const priceInput = document.getElementById("item-price")
   priceInput.addEventListener("input", () => {
-    const addTaxDom = document.getElementById("add-tax-price");
-    addTaxDom.innerHTML = Math.round(priceInput.value * 0.1 );
-    const addPofitDom = document.getElementById("profit");
-    addPofitDom.innerHTML = Math.round(priceInput.value - Math.round(priceInput.value * 0.1 ))
-  })
+  const inputValue = priceInput.value;
+  const addTaxDom = document.getElementById("add-tax-price")
+  addTaxDom.innerHTML = Math.floor(inputValue/10)
+  const addLaxDom = document.getElementById("profit")
+  addLaxDom.innerHTML = Math.floor(inputValue-inputValue/10)
+})
 });
