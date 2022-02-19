45eb26fb869c277ea62b733bb13e7d0b1e1d5208
Kazuki10peace <kazuki10asobo@ezweb.ne.jp>
2022-02-19 10:57:20 +0900

    売却後の編集ページへの遷移修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,11 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to root_path unless current_user.id == @item.user.id
+    if current_user.id == !@item.user.id && @item.order_history == nil
+      render :edit
+    else
+      redirect_to root_path 
+    end
   end
 
   def update
