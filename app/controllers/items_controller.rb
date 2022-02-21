f8baf2ea7c3d92c4c18773224211816061d98ffe
Kazuki10peace <kazuki10asobo@ezweb.ne.jp>
2022-02-21 10:33:29 +0900

    itemsコントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.order('created_at DESC')
@@ -23,7 +23,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if current_user.id == !@item.user.id || @item.order_history.nil?
+    if current_user.id == @item.user.id || @item.order_history.present?
       render :edit
     else
       redirect_to root_path
@@ -39,9 +39,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    if current_user.id == item.user.id
-      item.destroy
+    if current_user.id == @item.user.id
+      @item.destroy
       redirect_to root_path
     else
       redirect_to root_path
