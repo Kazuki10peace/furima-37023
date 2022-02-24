e0ce0680dc987719222c2e5a187171e57068e990
Kazuki10peace <kazuki10asobo@ezweb.ne.jp>
2022-02-24 11:59:04 +0900

    新規登録画面のエラーメッセージを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
+  validates :nickname, numericality: { presence: true, message: "を入力してください"}
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX
+  validates_format_of :password, with: PASSWORD_REGEX, message: "を入力してください"
   NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/i.freeze
-  validates_format_of :last_name, with: NAME_REGEX
-  validates_format_of :first_name, with: NAME_REGEX
+  validates_format_of :last_name, with: NAME_REGEX, message: "を正しく入力してください"
+  validates_format_of :first_name, with: NAME_REGEX, message: "を正しく入力してください"
   KANA_REGEX = /\A[ァ-ヶー－]+\z/i.freeze
-  validates_format_of :last_name_kana, with: KANA_REGEX
-  validates_format_of :first_name_kana, with: KANA_REGEX
-  validates :birth_date, presence: true
+  validates_format_of :last_name_kana, with: KANA_REGEX, message: "を正しく入力してください"
+  validates_format_of :first_name_kana, with: KANA_REGEX, message: "を正しく入力してください"
+  validates :birth_date, numericality: { presence: true, message: "を入力してください"}
 
   has_many :items
   has_many :order_histories
