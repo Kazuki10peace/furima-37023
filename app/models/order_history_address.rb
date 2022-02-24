51c8097474aeb8499d2bcbf613a2005601d362e4
Kazuki10peace <kazuki10asobo@ezweb.ne.jp>
2022-02-24 11:50:30 +0900

    購入画面のエラーメッセージ編集

--- app/models/order_history_address.rb
+++ app/models/order_history_address.rb
@@ -2,16 +2,14 @@ class OrderHistoryAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :token, :prefecture_id, :city, :addresses, :building, :phone_number, :user_id, :item_id, :token
 
-  with_options presence: true do
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
-    validates :prefecture_id, numericality: { other_than: 1, message: "を入力してください" }
-    validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :addresses
-    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
-    validates :user_id
-    validates :item_id
-    validates :token
-  end
+  validates :postal_code, presence: true, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "を正しく入力してください"}
+  validates :prefecture_id, numericality: { other_than: 1, message: "を入力してください" }
+  validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "を正しく入力してください"}
+  validates :addresses, numericality: { presence: true, message: "を入力してください"}
+  validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "を正しく入力してください" }
+  validates :user_id, numericality: { presence: true, message: "を入力してください"}
+  validates :item_id, numericality: { presence: true, message: "を入力してください"}
+  validates :token, numericality: { presence: true, message: "を入力してください"}
 
   def save
     order_history = OrderHistory.create(user_id: user_id, item_id: item_id)
