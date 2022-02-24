34072b335b7a4f6e3ca5fcf97cccaaaf709622a1
Kazuki10peace <kazuki10asobo@ezweb.ne.jp>
2022-02-24 12:14:34 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,19 @@
 class User < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, numericality: { presence: true, message: "を入力してください"}
+  validates :nickname, presence: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  validates :password, numericality: { with: PASSWORD_REGEX, message: "を入力してください"}
+  validates_format_of :password, with: PASSWORD_REGEX
   NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/i.freeze
-  validates :last_name, numericality: { with: NAME_REGEX, message: "を正しく入力してください"}
-  validates :first_name, numericality: { with: NAME_REGEX, message: "を正しく入力してください"}
+  validates_format_of :last_name, with: NAME_REGEX
+  validates_format_of :first_name, with: NAME_REGEX
   KANA_REGEX = /\A[ァ-ヶー－]+\z/i.freeze
-  validates :last_name_kana, numericality: { with: KANA_REGEX, message: "を正しく入力してください"}
-  validates :first_name_kana, numericality: { with: KANA_REGEX, message: "を正しく入力してください"}
-  validates :birth_date, numericality: { presence: true, message: "を入力してください"}
+  validates_format_of :last_name_kana, with: KANA_REGEX
+  validates_format_of :first_name_kana, with: KANA_REGEX
+  validates :birth_date, presence: true
 
   has_many :items
   has_many :order_histories
